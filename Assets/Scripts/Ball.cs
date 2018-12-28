c59a75cb92eaa3db12e1aea0f803a33133ef42b9
Kuyul <peyopeyo92@gmail.com>
2018-12-28 14:09:29 +1300

    Merge2

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,183 @@
+<<<<<<< HEAD
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    //Declare public variables
+    public GameObject ArrowGO;
+
+    public Transform Arrow;
+    public float ballForce;
+    public bool freeBall;
+    public bool flying;
+    public float PerfectBoundary = 10.0f; //Perfect boundary is +-n from the calculated perfect angle
+
+    //Declare Private variables
+    private GameObject CurrentCircle;
+    private Animator ArrowAnimator;
+    private Vector2 dir;
+    private Vector2 newDir;
+    private Rigidbody2D rb;
+    private bool clockwise;
+    private bool within = false;
+    private int passedCount = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        flying = true;
+        ArrowAnimator = ArrowGO.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // arrow follows balls position
+        Arrow.transform.position = transform.position;
+
+        // calculates angle between reference vector and perpendicular vector to change direction of arrow
+        Vector2 orig = new Vector2(0, 1);
+        var m_Angle = Vector2.SignedAngle(orig, newDir);
+        Arrow.transform.rotation = Quaternion.Euler(0, 0, m_Angle);
+
+        // calculates the angle between the ball and the circle's transform with reference to (0,1) vector
+        var angle = Vector2.SignedAngle(orig, dir);
+        //adjust angle to same orientation as perfectangles
+        if(angle > 0)
+        {
+            angle -= 360; //i.e. if angle = 150 thats -210
+        }
+
+        if (CurrentCircle != null)
+        {
+            var PerfectAngle = CurrentCircle.GetComponent<Circles>().GetPerfectAngle();
+            var Boundary1 = PerfectAngle - PerfectBoundary;//First Boundary
+            if (Boundary1 <= -360)
+            {
+                Boundary1 += 360; //Adjust angle it needs to be within 0 ~ -360
+            }
+            var Boundary2 = PerfectAngle + PerfectBoundary; //Second Boundary
+
+            if (angle <= Boundary2 && angle >= Boundary1)
+            {
+                //Only if the ball is within the pefect zone
+                if (!within)
+                {
+                    EnterPerfectZone();
+                }
+            }
+            else
+            {
+                //If the ball is outside the perfect zone
+                if (within)
+                {
+                    LeavePerfectZone();
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Contains("ring"))
+        {
+          //  passedCount = 0; //Reset passed count
+            //Need to declare current circle for reference in CameraScript
+            CurrentCircle = collision.gameObject;
+            flying = false;
+            rb.velocity = new Vector3(0, 0, 0);
+            rb.isKinematic = true;
+            transform.SetParent(collision.transform);
+            ArrowGO.SetActive(true);
+            clockwise = CurrentCircle.GetComponent<Circles>().SpinDirection();
+
+            if (passedCount == 1)
+            {
+                GameControl.instance.IncrementScore();
+                passedCount = 0;
+            }
+            else
+            {
+                // exponential system
+                GameControl.instance.IncrementScore();
+                passedCount = 0;
+            }
+        }
+
+        //Detect how many circles that the ball flew over
+        if(collision.tag == "counter")
+        {
+            passedCount += 1;
+            FlyOver();
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        // works out direction perpendicular to balls spinning direction
+        dir = transform.position - collision.transform.position;
+        newDir = Vector2.Perpendicular(dir.normalized);
+
+        if (clockwise)
+        {
+            newDir = -newDir;
+        }
+        if (freeBall == true)
+        {
+            freeBall = false;
+            flying = true;
+            transform.SetParent(null);
+            rb.isKinematic = false;
+            rb.AddForce(newDir * ballForce);
+            ArrowGO.SetActive(false);
+        }
+    }
+
+    //Called from the CameraScript Class
+    public GameObject GetCurrentCircle()
+    {
+        return CurrentCircle;
+    }
+
+    private void EnterPerfectZone()
+    {
+        //TODO: enter perfect zone logic
+       // Debug.Log("Enter Perfect Zone");
+        ArrowAnimator.SetTrigger("Enlarge");
+        within = true;
+    }
+
+    private void LeavePerfectZone()
+    {
+        //TODO: leave perfect zone logic
+        //Debug.Log("Leave Perfect Zone");
+        ArrowAnimator.SetTrigger("Decrease");
+        within = false;
+    }
+
+    public void FreeBall()
+    {
+        freeBall = true;
+        if (within)
+        {
+            JumpWithinPerfect();
+            LeavePerfectZone();
+        }
+    }
+
+    public void JumpWithinPerfect()
+    {
+        Debug.Log("Perfect!!");
+    }
+
+    private void FlyOver()
+    {
+        Debug.Log("Flew over: " + passedCount + " circles");
+    }
+}
+>>>>>>> e01eeca7ae3d45bb02634c727df2b164aeca5a55
---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,153 @@
+<<<<<<< HEAD
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    //Declare public variables
+    public static GameControl instance;
+    public float AdjustPerfectClkwise = 15f;
+    public float AdjustPerfectAntiClkwise = 15f;
+
+    public int ringNumber;
+    public GameObject[] rings;
+    public float smallsmall;
+    public float smallmedium;
+    public float smalllarge;
+    public float mediumsmall;
+    public float mediummedium;
+    public float mediumlarge;
+    public float largesmall;
+    public float largemedium;
+    public float largelarge;
+    public GameObject Counter;
+
+    public Text textCurrentscore;
+
+    //Declare Private Variables
+    private List<GameObject> ringList = new List<GameObject>();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        // initial spawn of rings at origin
+        for (int i = 0; i < ringNumber; i++)
+        {
+            ringList.Add(Instantiate(rings[Random.Range(0,rings.Length)],new Vector3(0,0,0),Quaternion.identity));
+        }
+        // arranges rings by x offset depending on ring size
+        AddOffset();
+
+        //Create a counter object for each circle
+        for (int i = 1; i < ringNumber; i++)
+        {
+            Instantiate(Counter, ringList[i].transform.position - new Vector3(0.5f, 0, 0), Quaternion.identity);
+        }
+
+        textCurrentscore.text = PlayerPrefs.GetInt("score", 0).ToString();
+    }
+
+    public void Dead()
+    {
+        PlayerPrefs.SetInt("score", 0);
+        StartCoroutine(Delay(1f));
+    }
+
+    IEnumerator Delay(float time)
+    {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(0);
+    }
+
+    // increments level by 1 when stage clear
+    public void LevelClear()
+    {
+        PlayerPrefs.SetInt("level", PlayerPrefs.GetInt("level", 1) + 1);
+        StartCoroutine(Delay(1f));
+    }
+
+    public void IncrementScore()
+    {
+        PlayerPrefs.SetInt("score", PlayerPrefs.GetInt("score", 0) + 1);
+        textCurrentscore.text = PlayerPrefs.GetInt("score", 0).ToString();
+    }
+
+    public void ExponentialScore(int passedcount)
+    {
+        // exponential algooorithm coem up with here.
+        PlayerPrefs.SetInt("score", PlayerPrefs.GetInt("score", 0) + 1);
+        textCurrentscore.text = PlayerPrefs.GetInt("score", 0).ToString();
+    }
+
+    private void AddOffset()
+    {
+        // arranges rings by x offset depending on ring size
+        for (int i = 0; i < ringNumber - 1; i++)
+        {
+            if (ringList[i].tag == "ringsmall") // current ring size
+            {
+                if (ringList[i + 1].tag == "ringsmall") // next ring size
+                {
+                    ringList[i + 1].transform.position = new Vector3(ringList[i].transform.position.x,0,0) + new Vector3(smallsmall, Random.Range(-2f, 2f), 0);
+                }
+                else if (ringList[i + 1].tag == "ringmedium") // next ring size
+                {
+                    ringList[i + 1].transform.position = new Vector3(ringList[i].transform.position.x, 0, 0) + new Vector3(smallmedium, Random.Range(-2f, 2f), 0);
+                }
+                else if (ringList[i + 1].tag == "ringlarge") // next ring size
+                {
+                    ringList[i + 1].transform.position = new Vector3(ringList[i].transform.position.x, 0, 0) + new Vector3(smalllarge, Random.Range(-2f, 2f), 0);
+                }
+
+            }
+            else if (ringList[i].tag == "ringmedium")
+            {
+                if (ringList[i + 1].tag == "ringsmall") // next ring size
+                {
+                    ringList[i + 1].transform.position = new Vector3(ringList[i].transform.position.x, 0, 0) + new Vector3(mediumsmall, Random.Range(-2f, 2f), 0);
+                }
+                else if (ringList[i + 1].tag == "ringmedium") // next ring size
+                {
+                    ringList[i + 1].transform.position = new Vector3(ringList[i].transform.position.x, 0, 0) + new Vector3(mediummedium, Random.Range(-2f, 2f), 0);
+                }
+                else if (ringList[i + 1].tag == "ringlarge") // next ring size
+                {
+                    ringList[i + 1].transform.position = new Vector3(ringList[i].transform.position.x, 0, 0) + new Vector3(mediumlarge, Random.Range(-2f, 2f), 0);
+                }
+            }
+            else if (ringList[i].tag == "ringlarge")
+            {
+                if (ringList[i + 1].tag == "ringsmall") // next ring size
+                {
+                    ringList[i + 1].transform.position = new Vector3(ringList[i].transform.position.x, 0, 0) + new Vector3(largesmall, Random.Range(-2f, 2f), 0);
+                }
+                else if (ringList[i + 1].tag == "ringmedium") // next ring size
+                {
+                    ringList[i + 1].transform.position = new Vector3(ringList[i].transform.position.x, 0, 0) + new Vector3(largemedium, Random.Range(-2f, 2f), 0);
+                }
+                else if (ringList[i + 1].tag == "ringlarge") // next ring size
+                {
+                    ringList[i + 1].transform.position = new Vector3(ringList[i].transform.position.x, 0, 0) + new Vector3(largelarge, Random.Range(-2f, 2f), 0);
+                }
+            }
+            //Calculate the perfect angle
+            ringList[i].GetComponent<Circles>().AddNextCircle(ringList[i + 1]);
+        }
+    }
+}
+>>>>>>> e01eeca7ae3d45bb02634c727df2b164aeca5a55
